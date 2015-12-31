dcb49c92e47517398d4d5527e4aedf6104fd2346
Sean McGivern <sean.mcgivern@rightscale.com>
2015-12-31 12:33:50 +0000

    Use netrc for credentials
    Before: credentials passed on the command line would override those in
    the env vars.
    
    After: credentials in env vars override those found in ~/.netrc, which
    is supposed to be the canonical source for the credentials.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ extern crate cookie;
 extern crate docopt;
 #[macro_use] extern crate hyper;
 extern crate libc;
+extern crate netrc;
 extern crate rustc_serialize;
 
 use std::env;
+use std::io::BufReader;
 use std::io::prelude::*;
+use std::fs::File;
 
 use docopt::Docopt;
+use netrc::Netrc;
 
 #[macro_use] mod die;
 mod rightscale_api;
@@ -21,22 +25,30 @@ Usage: rsssh [connect] <host> [options]
        rsssh
 
 Options:
-    -h, --help                   show this help
-    -c, --config=<config-file>   use alternative config file [default: ~/.ssh/rsssh_config.toml]
-    --account=<account-id>       the account ID to use when searching for a host
-    --server=<server-name>       name of server or array; can use as a wildcard
-    --user=<user>                user to switch to after connect
-    --command=<command>          command to run after connect (must include a shell; try suffixing `&& /bin/bash`)
-    --email=<email>              email to use to connect to the RightScale API (or set RSSSH_EMAIL)
-    --password=<password>        password to use to connect to the RightScale API (or set RSSSH_PASSWORD)
+    -h, --help                  show this help
+    -c, --config=<config-file>  use alternative config file [default: ~/.ssh/rsssh_config.toml]
+    --account=<account-id>      the account ID to use when searching for a host
+    --server=<server-name>      name of server or array; can use as a wildcard
+    --user=<user>               user to switch to after connect
+    --command=<command>         command to run after connect (must include a shell; try suffixing `&& /bin/bash`)
+
+To pass credentials for connecting to the RightScale API, either:
+1. Create a ~/.netrc file with an entry for the machine 'rsssh'. (Recommended.)
+2. Set the environment variables RSSSH_EMAIL and RSSSH_PASSWORD.
+
+The environment variables will override any values set in the netrc file.
 ";
 
 const ERROR_MISSING_CREDENTIALS: &'static str = "
-Email and password are both required to connect to the RightScale API. Either pass them on
-the command line:
-    --email=<email> --password=<password>
+Email and password are both required to connect to the RightScale API. Either set the
+environment variables RSSSH_EMAIL and RSSSH_PASSWORD, or create a ~/.netrc file with an
+entry for 'rsssh', like so:
+    machine rsssh
+      login email@example.com
+      password MyPassword
 
-or set the environment variables RSSSH_EMAIL and RSSSH_PASSWORD.
+For more information, see:
+  http://www.gnu.org/software/inetutils/manual/html_node/The-_002enetrc-file.html
 ";
 
 const ERROR_ACCOUNT_SERVER_REQUIRED: &'static str = "
@@ -56,8 +68,12 @@ struct Args {
     flag_server: Option<String>,
     flag_user: Option<String>,
     flag_command: Option<String>,
-    flag_email: Option<String>,
-    flag_password: Option<String>,
+}
+
+#[derive(Debug)]
+struct Credentials {
+    email: Option<String>,
+    password: Option<String>,
 }
 
 fn main() {
@@ -87,8 +103,9 @@ fn delete(host: &str, config: &str) {
 }
 
 fn connect(args: Args) {
-    let email = default_from_env(args.flag_email, "RSSSH_EMAIL");
-    let password = default_from_env(args.flag_password, "RSSSH_PASSWORD");
+    let netrc = read_netrc("~/.netrc");
+    let email = override_from_env(netrc.email, "RSSSH_EMAIL");
+    let password = override_from_env(netrc.password, "RSSSH_PASSWORD");
     let account = args.flag_account;
     let server = args.flag_server;
     let user = args.flag_user;
@@ -107,30 +124,76 @@ fn connect(args: Args) {
     }
 }
 
-fn default_from_env(value: Option<String>, env_var: &str) -> Option<String> {
-    value.or(env::var_os(env_var).and_then(|s| Some(s.to_str().unwrap().to_string())))
+fn override_from_env(value: Option<String>, env_var: &str) -> Option<String> {
+    env::var_os(env_var).and_then(|s| Some(s.to_str().unwrap().to_string())).or(value)
+}
+
+fn option_string(string: String) -> Option<String> {
+    if string.is_empty() {
+        None
+    } else {
+        Some(string)
+    }
+}
+
+fn find_rsssh_host(hosts: Vec<(String, netrc::Machine)>) -> Option<Credentials> {
+    hosts
+        .into_iter()
+        .find(|host| host.0 == "rsssh")
+        .map(|host| Credentials { email: option_string(host.1.login), password: host.1.password })
+}
+
+fn read_netrc(path: &str) -> Credentials {
+    let file = File::open(expand_home_directory(path));
+    let no_credentials = Credentials { email: None, password: None };
+    let result = file
+        .map(|file| BufReader::new(file))
+        .map(|buffer| Netrc::parse(buffer).map(|netrc| netrc.hosts).unwrap_or(Vec::new()))
+        .map(find_rsssh_host);
+
+    match result {
+        Ok(r) => r.unwrap_or(no_credentials),
+        Err(e) => {
+            println!("Error finding config: {:?}", e);
+            no_credentials
+        }
+    }
+}
+
+fn expand_home_directory(path: &str) -> String {
+    match env::var_os("HOME") {
+        Some(s) => path.to_string().replace("~", s.to_str().unwrap()),
+        None => path.to_string()
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    mod default_from_env {
-        use super::super::default_from_env;
+    mod override_from_env {
+        use super::super::override_from_env;
 
         #[test]
-        fn value_passed() {
+        fn both_exist() {
             let value = Some("test@example.com".to_string());
 
-            assert_eq!(value.clone(), default_from_env(value, "CARGO_MANIFEST_DIR"));
+            assert_eq!(Some(env!("CARGO_MANIFEST_DIR").to_string()), override_from_env(value, "CARGO_MANIFEST_DIR"));
+        }
+
+        #[test]
+        fn value_exists() {
+            let value = Some("test@example.com".to_string());
+
+            assert_eq!(value.clone(), override_from_env(value, "ENV_VAR_THAT_DOES_NOT_EXIST"));
         }
 
         #[test]
         fn env_exists() {
-            assert_eq!(Some(env!("CARGO_MANIFEST_DIR").to_string()), default_from_env(None, "CARGO_MANIFEST_DIR"));
+            assert_eq!(Some(env!("CARGO_MANIFEST_DIR").to_string()), override_from_env(None, "CARGO_MANIFEST_DIR"));
         }
 
         #[test]
         fn neither_exists() {
-            assert_eq!(None, default_from_env(None, "ENV_VAR_THAT_DOES_NOT_EXIST"));
+            assert_eq!(None, override_from_env(None, "ENV_VAR_THAT_DOES_NOT_EXIST"));
         }
     }
 }
