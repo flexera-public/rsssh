0ba3561e1f3d9290e886c9634e5283c88729f83b
Sean McGivern <sean.mcgivern@rightscale.com>
2016-01-04 10:17:49 +0000

    Only expand tilde at start of path
    The tilde only gets expanded by a shell at the start of a path, so do
    the same here.

--- src/config.rs
+++ src/config.rs
@@ -15,9 +15,17 @@ pub struct Credentials {
 }
 
 fn expand_home_directory(path: &str) -> String {
-    match env::var_os("HOME") {
-        Some(s) => path.to_string().replace("~", s.to_str().unwrap()),
-        None => path.to_string()
+    if path == "" { return path.to_string(); }
+
+    let (prefix, rest) = path.split_at(1);
+    let home_dir = env::home_dir();
+
+    if prefix == "~" {
+        let expanded_prefix = home_dir.as_ref().and_then(|s| s.to_str()).unwrap_or("~");
+
+        [expanded_prefix, rest].concat()
+    } else {
+        path.to_string()
     }
 }
 
@@ -81,6 +89,46 @@ pub fn read_netrc(path: &str) -> Credentials {
         Err(e) => {
             println!("Error reading netrc file {}: {:?}", path, e);
             no_credentials
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    mod expand_home_directory {
+        use super::super::expand_home_directory;
+        use std::env;
+
+        fn home_dir() -> String {
+            env::home_dir().unwrap().to_str().unwrap().to_string()
+        }
+
+        #[test]
+        fn expand_no_path() {
+            assert_eq!("".to_string(), expand_home_directory(""))
+        }
+
+        #[test]
+        fn expand_tilde_prefix() {
+            assert_eq!(home_dir() + "/.ssh/rsssh_config.toml",
+                       expand_home_directory("~/.ssh/rsssh_config.toml"))
+        }
+
+        #[test]
+        fn expand_tilde_suffix() {
+            assert_eq!("/.ssh/rsssh_config.toml~".to_string(),
+                       expand_home_directory("/.ssh/rsssh_config.toml~"))
+        }
+
+        #[test]
+        fn expand_tilde_tilde() {
+            assert_eq!(home_dir() + "~", expand_home_directory("~~"))
+        }
+
+        #[test]
+        fn expand_no_tilde() {
+            assert_eq!("/home/sean/.ssh/rsssh_config.toml".to_string(),
+                       expand_home_directory("/home/sean/.ssh/rsssh_config.toml"))
         }
     }
 }
