a2c3842546181ff4999ea8b1b919ea6d63542d59
Sean McGivern <sean.mcgivern@rightscale.com>
2016-01-04 15:11:52 +0000

    Use correct shard when getting instances
    1. Logging in can redirect to a different shard. Hyper's default follow
       redirection policy doesn't keep POST data, so implement that manually
       to support accounts on the us-4 shard.
       <https://github.com/hyperium/hyper/issues/641>
    
    2. The RightScale API 1.5 redirects clients to the correct
       shard. However, Hyper _also_ doesn't keep cookies on redirection, so
       go the shard given on login instead.
       <https://github.com/hyperium/hyper/issues/344>
    
    3. As it happens, the RightScale API 1.6 (which is faster) doesn't
       redirect to the correct shard anyway, so both Hyper and API 1.6 need
       changes before we can just hit 'my.rightscale.com' and get the right
       results.

--- src/rightscale_api.rs
+++ src/rightscale_api.rs
@@ -1,6 +1,6 @@
 use cookie::CookieJar;
-use hyper::client::Client;
-use hyper::header::{Cookie, SetCookie};
+use hyper::client::{Client, RedirectPolicy};
+use hyper::header::{Cookie, SetCookie, Location};
 use hyper::status::StatusCode;
 use rustc_serialize::json::Json;
 use std::io::prelude::*;
@@ -8,45 +8,58 @@ use std::io::prelude::*;
 header! { (XApiVersion, "X-Api-Version") => [String] }
 header! { (XAccount, "X-Account") => [i64] }
 
-fn log_in<'a>(email: &str, password: &str, account: i64, verbose: bool) -> CookieJar<'a> {
-    let client = Client::new();
+fn log_in<'a>(url: &str, email: &str, password: &str, account: i64, verbose: bool) -> (String, CookieJar<'a>) {
+    let mut client = Client::new();
     let api_15 = XApiVersion("1.5".to_string());
-    let url = "https://my.rightscale.com/api/sessions";
     let params = &format!("email={}&password={}&account_href=/api/accounts/{}", email, password, account);
 
-    if verbose { println!("Logging in to RightScale API at {} with parameters: {}", url, params) }
+    if verbose { println!("Logging in to RightScale API at {} with parameters: {:?}", url, params) }
 
-    let login_request = client.post(url).header(api_15).body(params).send().unwrap();
+    client.set_redirect_policy(RedirectPolicy::FollowNone);
+
+    let login_response = client.post(url).header(api_15).body(params).send().unwrap();
     let mut cookie_jar = CookieJar::new(b"secret");
 
-    if login_request.status != StatusCode::NoContent {
-        die!("Failed to log in to the RightScale API, got response: {}", login_request.status)
-    }
+    match login_response.status {
+        StatusCode::NoContent => {
+            login_response.headers.get::<SetCookie>().unwrap().apply_to_cookie_jar(&mut cookie_jar);
 
-    login_request.headers.get::<SetCookie>().unwrap().apply_to_cookie_jar(&mut cookie_jar);
-    cookie_jar
+            (login_response.url.domain().unwrap().to_string(), cookie_jar)
+        },
+        StatusCode::Found => match login_response.headers.get::<Location>() {
+            Some(location) => log_in(&format!("{}", location), email, password, account, verbose),
+            _ => die!("Couldn't find location header for response: {:?}", login_response)
+        },
+        s => die!("Failed to log in to the RightScale API: {}", s)
+    }
 }
 
 pub fn find_ip(email: &str, password: &str, account: i64, server: &str, exact_match: bool, verbose: bool) -> String {
+    let login_url = "https://my.rightscale.com/api/sessions";
     let client = Client::new();
-    let cookie_jar = log_in(email, password, account, verbose);
+    let (shard, cookie_jar) = log_in(login_url, email, password, account, verbose);
     let server_name = if exact_match { server.to_string() } else { format!("%25{}%25", server) };
     let api_16 = XApiVersion("1.6".to_string());
     let x_account = XAccount(account);
     let cookie = Cookie::from_cookie_jar(&cookie_jar);
-    let url = format!("https://my.rightscale.com/api/instances?filter=name%3D{}%26state%3Doperational", server_name);
+    let url = format!("https://{}/api/instances?filter=name%3D{}%26state%3Doperational", shard, server_name);
+    let mut body = String::new();
 
     if verbose { println!("Finding server: {}", url) }
 
-    let mut find = client.get(&url).header(api_16).header(x_account).header(cookie).send().unwrap();
-    let mut body = String::new();
+    let mut response = client.get(&url).header(api_16).header(x_account).header(cookie).send().unwrap();
 
-    match find.read_to_string(&mut body) {
-        Ok(_) => (),
-        Err(e) => die!("Error reading response from RightScale API: {}", e)
-    }
-
-    let result = Json::from_str(&body);
+    let result = match response.status {
+        StatusCode::Ok => {
+            match response.read_to_string(&mut body) {
+                Ok(_) => Json::from_str(&body),
+                Err(e) => {
+                    die!("Error reading response from RightScale API: {}", e)
+                }
+            }
+        },
+        _ => die!("Unexpected response from RightScale API: {}", response.status)
+    };
 
     if let Ok(result) = result {
         let first_ip = result.as_array()
@@ -62,6 +75,6 @@ pub fn find_ip(email: &str, password: &str, account: i64, server: &str, exact_ma
             None => die!("Couldn't find server IP. API response: {:?}", result)
         }
     } else {
-        die!("Error parsing response from RightScale API: {}", result.err().unwrap());
+        die!("Error parsing JSON response from RightScale API: {}", result.err().unwrap());
     }
 }
